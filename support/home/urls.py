ddf8b352530609bf4fe75080c642429cf72cf36b
noor shalaby <nourshalaby16@gmail.com>
2020-11-16 04:04:22 +0200

    add create html

--- support/home/urls.py
+++ support/home/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import view_index  , view_home
+from home.views import view_index  , view_home , create_home
 
  
 
 urlpatterns = [
     path('' , view_index),
     path('home' , view_home),
+    path('createhome',create_home)
 ]
