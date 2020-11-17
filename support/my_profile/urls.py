23cd3147fb9ad01ab50f632221f7bdf0b5ce7bc6
Amr Hassan <amr_hassan522@yahoo.com>
2020-11-17 23:49:42 +0200

    override

--- support/my_profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from my_profile.views import view_profile,edit_profile
- 
-urlpatterns = [
-    path('' ,view_profile),
-    path('edit.html',edit_profile)
-]
--- support/my_profile/views.py
+++ support/my_profile/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render , redirect
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def view_profile(request):
-    return render(request ,'profile.html')
-
-
-def edit_profile(request):
-    return render(request,'edit.html')
