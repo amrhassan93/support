2db3a5eb67680312b031428232946d6cd2b17ca2
noor shalaby <nourshalaby16@gmail.com>
2020-11-17 12:10:41 +0200

    add home design

--- support/home/urls.py
+++ support/home/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import view_index  , view_home , create_home
-
+from home.views import view_index  , view_home
  
 
 urlpatterns = [
     path('' , view_index),
     path('home' , view_home),
-    path('createhome',create_home)
+    # path('createhome',create_home)
 ]
