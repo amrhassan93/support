80635690acce35a225fa335d34291e0ef930e604
moomenrazik <37555587+moomenrazik@users.noreply.github.com>
2020-11-17 11:47:55 +0200

    Login And Signup full

--- support/reglogin/views.py
+++ support/reglogin/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-# from auth.models import Book , Auther
+# from {}.models import 
 
 # Create your views here.
 def view_login(request):
     return render(request ,'login.html')
+
+def view_signup(request):
+    return render(request ,'signup.html')
+
+def upload(request):
+    return render(request ,'')
