b84da9f0e8a77e41383c102f7ad6c9c26c3020e6
Mohamed Yasser <egyria2011@gmail.com>
2020-11-17 15:00:48 +0200

    uploading my_profile

---
+++ support-master/support-master/support/home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from home.views import view_index  , view_home
+
+ 
+
+urlpatterns = [
+    path('' , view_index),
+    path('home' , view_home),
+]
