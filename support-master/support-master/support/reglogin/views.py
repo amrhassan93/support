9262fee1b5ced06a87ec696169d365781b024755
Amr Hassan <amr_hassan522@yahoo.com>
2020-11-17 21:13:34 +0200

    delete mohamed commit

--- support-master/support-master/support/reglogin/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render , redirect
-from django.http import HttpResponse
-# from auth.models import Book , Auther
-
-# Create your views here.
-def view_login(request):
-    return render(request ,'login.html')
