a6c0686b351e442447b6d1477431495a93f40f2a
Mohamed Yasser <egyria2011@gmail.com>
2020-11-17 23:12:45 +0200

    edit

---
+++ support/support/my_profile/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from my_profile.views import view_profile,edit_profile
+ 
+urlpatterns = [
+    path('' ,view_profile),
+    path('edit.html',edit_profile)
+]
