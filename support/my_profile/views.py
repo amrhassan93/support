c83daef56bef55e3ff579b95048d6f491e04d7c5
Amr Hassan <amr_hassan522@yahoo.com>
2020-11-23 19:57:43 +0200

    delete imgs

--- support/my_profile/views.py
+++ support/my_profile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-from project.models import *
+from projects.models import Project
 from reglogin.models import Users
 
 # Create your views here.
