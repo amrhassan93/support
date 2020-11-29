ea41a961b9e12c78989c19b1fbc50d392742d955
noor shalaby <nourshalaby16@gmail.com>
2020-11-29 19:13:54 +0200

    final work

--- support/home/views.py
+++ support/home/views.py
@@ -1,40 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render ,  get_object_or_404
 from projects.models import Project
-# from django.db.models import Q
+
 # Create your views here.
 
-def view_index(request):
-    projects = Project.objects.all()
-    highest_five_rated  = Project.objects.all()[:5]
-
-    lastest_five = Project.objects.order_by('-start_time')[:5]
-
-    featured_projects = Project.objects.filter(Featured = True)
-    
-    context = {
-        'highest_five_rated' : highest_five_rated,
-        'lastest_five' : lastest_five,
-        'featured_projects': featured_projects
-        }
-
-    return render(request,'home/index.html',context)
-
-
 def view_home(request):
-    return render(request , 'home/home.html')
+     highest_five_rated = Project.objects.all()[:5]
+     lastest_five = Project.objects.order_by('created_at')[:5]
+     featured_projects = Project.objects.filter(is_featured = True)
+     context = {
+          'highest_five_rated': highest_five_rated,
+          'lastest_five': lastest_five,
+          'featured_projects': featured_projects
+      }
+     return render(request,'home/home.html',context)
 
-# def create_home(request):
-#     return render(request, 'home/create_home.html')    
+
+def view_index(request):
+    return render(request , 'home/index.html')
 
 
- # # 'title' : title,
-        # # 'details': details,
-        # # 'target': target,
-        # # 'created_at': created_at,
-        # # 'start_date': start_date,
-        # # 'end_date': end_date,
-        # # 'images': images,
-        # # 'total_rate': total_rate,
-        # # 'is_featured': is_featured,
-        # # 'user_id': user_id
-        # 'projects':projects
+def search(request , keyword):
+    search_result =get_object_or_404(Project ,title = keyword)
+    #  Project.objects.get(title = keyword)
+    return render(request , 'home/search.html' ,{"search_result" : search_result})
+        
--- support/projects/admin.py
+++ support/projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import  Users,ProjectTag, Project, Comments, CommentReply, Donation, Rating
+
+admin.site.register(ProjectTag)
+admin.site.register(Project)
+admin.site.register(Comments)
+admin.site.register(CommentReply)
+admin.site.register(Donation)
+admin.site.register(Rating)
+admin.site.register(Users)
+
+
